4f2d111f3c10c292bfdb0b2d9e20ad9aae4fdc22
Baeri3000 <lars.stalder@gmail.com>
2024-01-16 11:34:12 +0100

    add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCoSbxPAZTD20LhTCuMDNmTULsbVkqFrrY",
+    authDomain: "cas-brick-breaker-vue.firebaseapp.com",
+    projectId: "cas-brick-breaker-vue",
+    storageBucket: "cas-brick-breaker-vue.appspot.com",
+    messagingSenderId: "928285454716",
+    appId: "1:928285454716:web:c9f03ccba594d2ad19b4f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
