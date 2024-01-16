eb76bc57f8ab7322bc55fd0867fd39298228462a
Baeri3000 <lars.stalder@gmail.com>
2024-01-16 12:08:48 +0100

    make canvas responsive

--- src/components/GameComponent.vue
+++ src/components/GameComponent.vue
@@ -1,11 +1,45 @@
 <template>
-  <div id="canvas-container">
-    <canvas id="gameCanvas" width="800" height="600"></canvas>
-  </div>
-  <div id="header-container">
-    <h1>Brick Breaker</h1>
-  </div>
+  <section id="game">
+    <div class="main-container">
+      <div class="content">
+        <div class="row">
+          <div class="col">
+            <div id="canvas-container">
+              <canvas id="gameCanvas" class="gameCanvas" width="800" height="600"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
+.main-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  text-align: center;
+  flex-direction: column;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.gameCanvas {
+  min-width: 800px;
+  min-height: 600px;
+}
 </style>
