9390ff08586bcc6a468c81b6b499b1455a0cc271
felberto <tobias.felber97@gmail.com>
2024-01-18 16:34:41 +0100

    try to set base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://cas-brick-breaker-vue.web.app/',
   plugins: [
     vue(),
   ],
