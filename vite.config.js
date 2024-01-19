9b78b104f67fc37aba82a9b813181d3597050a19
Baeri3000 <lars.stalder@gmail.com>
2024-01-19 16:01:26 +0100

    use own maskable icon

--- vite.config.js
+++ vite.config.js
@@ -30,10 +30,6 @@ export default defineConfig({
                     sizes: "512x512",
                     type: "image/png"
                 }, {
-                    src: "/img/icons/android-chrome-512x512.png",
-                    sizes: "512x512",
-                    type: "image/png"
-                }, {
                     src: "/img/icons/maskable_icon.png",
                     sizes: "144x144",
                     type: "image/png",
