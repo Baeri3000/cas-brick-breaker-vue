db9a1d0bdecc8b5f305b84ebc6d9f132a3dca037
Baeri3000 <lars.stalder@gmail.com>
2024-01-19 15:07:04 +0100

    correctly use vite pwa

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,42 @@ import {fileURLToPath, URL} from 'node:url'
 
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        manifest: true,
+    },
     plugins: [
-        vue()
+        vue(),
+        VitePWA({
+            manifest: {
+                short_name: "cas_brick_breaker_vue",
+                name: "CAS Brick Breake Vue PWA",
+                start_url: "https://cas-brick-breaker-vue.web.app",
+                background_color: "#333333",
+                theme_color: "#333333",
+                display: "standalone",
+                scope: "https://cas-brick-breaker-vue.web.app",
+                icons: [{
+                    src: "/img/icons/brick-breaker.png",
+                    sizes: "512x512",
+                    type: "image/png"
+                }, {
+                    src: "/img/icons/android-chrome-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png"
+                }, {
+                    src: "/img/icons/maskable_icon.png",
+                    sizes: "144x144",
+                    type: "image/png",
+                    purpose: "maskable"
+                }]
+            },
+            includeManifestIcons: true,
+            includeAssets: ['./public/img/icons/*.png']
+        })
     ],
     resolve: {
         alias: {
