2137b11e19c86d8501226a248b33eb9dd2a4fe0f
felberto <tobias.felber97@gmail.com>
2024-01-19 13:08:30 +0100

    add build manifest true

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,14 @@ import {fileURLToPath, URL} from 'node:url'
 
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        manifest: true,
+    },
     plugins: [
-        vue(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            devOptions: {
-                enabled: true
-            }
-        })
+        vue()
     ],
     resolve: {
         alias: {
